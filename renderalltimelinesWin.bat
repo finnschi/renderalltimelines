abb003b5b5d043508e6771a5fef6db22a195fb5e
finn.jager <Iamnotinwandsbek10!>
2021-02-18 22:50:54 +0100

    add Windows support

--- renderalltimelinesWin.bat
+++ renderalltimelinesWin.bat
@@ -1,21 +1,13 @@
 ::written by Finn Jager 2021
-::resolveStartupscript to set Environment to hook into the API  windows not working at all..  this is WIP
-
+::resolveStartupscript to set Environment to hook into the API for wandoze
+@echo off
 set RESOLVE_SCRIPT_API=%PROGRAMDATA%\\Blackmagic Design\\DaVinci Resolve\\Support\\Developer\\Scripting\\
 set RESOLVE_SCRIPT_LIB=C:\\Program Files\\Blackmagic Design\\DaVinci Resolve\\fusionscript.dll
 set PYTHONPATH=%RESOLVE_SCRIPT_API%\\Modules\\;%RESOLVE_SCRIPT_API%\\Examples\\
 
-echo %PYTHONPATH%
+set /p ans=Enter the name of the Render Preset:
+echo
+echo %ans%
 
-C:\Python27\python.exe formatchecker.py
-
-::echo "dirname: `dirname $0`"
-::parent=`dirname $0`
-
-
-::printf '%s ' 'Enter the name of the Render Preset'
-::read ans
-::echo 'Render is starting! for preset: ' $ans
-::
-::python $parent/renderalltimelines2.py $ans
+python %~dp0/renderalltimelines2.py %ans%
 
